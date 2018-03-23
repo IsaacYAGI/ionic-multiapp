7b6e28749c486d8dd47d0a04e219fca9ca75ef20
Isaac Yriarte <isaacyriarte@gmail.com>
2018-03-23 13:00:57 -0500

    Se muestra el mapa con la ubicacion actual del usuario y un pin

--- src/pages/mapviewver/mapviewver.ts
+++ src/pages/mapviewver/mapviewver.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController,  } from 'ionic-angular';
 import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation';
 
+declare var google;
 /**
  * Generated class for the MapviewverPage page.
  *
@@ -18,6 +19,8 @@ export class MapviewverPage {
 
   options : GeolocationOptions;
   currentPos : Geoposition;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(private geolocation : Geolocation, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -35,18 +38,56 @@ export class MapviewverPage {
     loading.present();
     
     this.options = {
-        enableHighAccuracy : true
+      enableHighAccuracy : true,
+      timeout:10000
     };
 
     this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
-        this.currentPos = pos;      
-        console.log(pos);
-        loading.dismiss();
+        
+    this.currentPos = pos;  
+
+    console.log(pos);
+    this.addMap(pos.coords.latitude,pos.coords.longitude);
+
+    loading.dismiss();
+
     },(err : PositionError)=>{
-        console.log("error : " + err.message);
-        loading.dismiss();
+      console.log("error : " + err.message);
+      loading.dismiss();
     });
-}
+  }
+  addMap(lat,long){
+
+    let latLng = new google.maps.LatLng(lat, long);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+
+  }
+
+  addMarker(){
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+      let content = "<p>Esta es tu posicion actual</p>";          
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 
 
 }
