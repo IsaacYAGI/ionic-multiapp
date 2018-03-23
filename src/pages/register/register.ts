e1ab37cfd5401e36ac08669ec87b50937487ce99
Isaac Yriarte <isaacyriarte@gmail.com>
2018-03-22 19:46:44 -0500

    Se inyectan las dependencias de firebase y se agrega el metodo de registro de usuarios

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseAuth } from '@firebase/auth-types';
 
 /**
  * Generated class for the RegisterPage page.
@@ -15,11 +17,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private firebase:AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  registrarUsuario(){
+    alert("Se registrará usuario.");
   }
 
 }
